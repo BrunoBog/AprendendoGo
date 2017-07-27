18af1004b8124d965999919a8a58176108066f1a
BOG <bog906@gmail.com>
2017-07-27 11:21:21 -0300

    Adicionando um tratamento de erro para o pacote

--- structAvancados/main.go
+++ structAvancados/main.go
@@ -10,12 +10,25 @@ func main() {
 	casa.X = 50
 	casa.Y = 60
 	casa.Nome = "Minha casa"
-	casa.Setvalor(75000)
+	if err := casa.Setvalor(12500000000); err != nil {
+		fmt.Println("[main] Houve um erro na atribuição do valor. \n ", err.Error())
+		if err == model.ErrValorMuitoAlto {
+			fmt.Println("[main]Por favor confira o valor digitado ou sua avaliação")
+		}
+
+		return
+	}
 
 	fmt.Println("O valor de casa é: ", casa)
 	fmt.Printf("O valor da casa é: %d\n", casa.GetValor)   // assim você chama o endereço de memoria
 	fmt.Printf("O valor da casa é: %d\n", casa.GetValor()) // assim você chama o valor
-	objJson, _ := json.Marshal(casa)                       // transforma a parada em json
-	// você usa o "_" quando quer ignorar um retorno
-	fmt.Println("O valor da casa em json", string(objJson))
+	//objJson, _ := json.Marshal(casa)                       // transforma a parada em json
+	// você usa o "_" ao invés do err quando quer ignorar o retorno do erro por exemplo
+	objJSON, err := json.Marshal(casa)
+	if err != nil {
+		fmt.Println("[main] Deu erro na hora de gerar o jSon T_T", err.Error)
+		return //panic se retornar panic o app fecha
+	}
+	fmt.Println("O valor da casa em json", string(objJSON))
+
 }
--- structAvancados/model/imovel.go
+++ structAvancados/model/imovel.go
@@ -1,5 +1,7 @@
 package model
 
+import "errors"
+
 //Imovel para exemplo de utilização de ponteiros e structs
 type Imovel struct {
 	Y     int    `json:"cordenada_x"`
@@ -8,9 +10,25 @@ type Imovel struct {
 	valor int    // campo não exportado então não pode atribuir etiqueta para json
 }
 
+var (
+	//ErrValorImovelErrado Varoavel para retorno de erro
+	ErrValorImovelErrado = errors.New("O valor informado é muito baixo")
+	//ErrValorMuitoAlto Erro devido ao valor do imovél superior a media
+	ErrValorMuitoAlto = errors.New("O valor informado é muito alto")
+)
+
 //Setvalor define o valor do imovel
-func (i *Imovel) Setvalor(valor int) {
+func (i *Imovel) Setvalor(valor int) (err error) {
 	i.valor = valor
+	err = nil
+	if i.valor < 50000 {
+		err = ErrValorImovelErrado
+		return
+	} else if i.valor > 1500000 {
+		err = ErrValorMuitoAlto
+		return
+	}
+	return
 }
 
 //GetValor retorna o valor do imovel
